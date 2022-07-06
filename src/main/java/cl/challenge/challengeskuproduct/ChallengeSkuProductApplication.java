98902337f4a2791edc690ca21c259705060e5b74
jesben sevillano <jesben.sevillano@xepelin.com>
2022-07-05 23:09:32 -0500

    feat: create project

---
+++ src/main/java/cl/challenge/challengeskuproduct/ChallengeSkuProductApplication.java
@@ -0,0 +1,13 @@
+package cl.challenge.challengeskuproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSkuProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeSkuProductApplication.class, args);
+    }
+
+}
